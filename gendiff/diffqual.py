059f3ac0965df9e837b9ac11a4c1e998893ff62e
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-06-13 22:56:16 +0300

    Step 5. Update diffqual.py.

--- gendiff/diffqual.py
+++ gendiff/diffqual.py
@@ -1,20 +1,22 @@
 import json
+from gendiff.file_parser import parser
 
 
 def gen_diff(file_1, file_2):
-    content_1 = json.load(open(file_1))
-    content_2 = json.load(open(file_2))
-    result = dict()
-    for key in content_1.keys() & content_2.keys():
-        if content_1[key] == content_2[key]:
-            result['  ' + key] = content_1[key]
+    content_1 = parser(file_1)
+    content_2 = parser(file_2)
+    diff = dict()
+    for key in sorted(content_1.keys() | content_2.keys()):
+        if key not in content_1 and key in content_2:
+            diff['+ ' + key] = content_2[key]
+        elif key in content_1 and key not in content_2:
+            diff['- ' + key] = content_1[key]
+        elif content_1[key] == content_2[key]:
+            diff['  ' + key] = content_1[key]
         else:
-            result['- ' + key] = content_1[key]
-            result['+ ' + key] = content_2[key]
-    for key in content_1.keys() - content_2.keys():
-        result['- ' + key] = content_1[key]
-    for key in content_2.keys() - content_1.keys():
-        result['+ ' + key] = content_2[key]
-    sorted_result = dict(sorted(result.items(), key=lambda item: item[0][2:]))
+            diff['- ' + key] = content_1[key]
+            diff['+ ' + key] = content_2[key]
+
     return json.dumps(
-        sorted_result, separators=('', ': '), indent=2).replace('"', '')
+        diff, separators=('', ': '), indent=2
+    ).replace('"', '')
