ad63ce087957774fb61443fc2887835b715ccd57
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-06-15 16:43:06 +0300

    Step 6. Add tests.

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -5,8 +5,20 @@ FLAT_JSON_1 = 'tests/fixtures/file1.json'
 FLAT_JSON_2 = 'tests/fixtures/file2.json'
 FLAT_YAML_1 = 'tests/fixtures/file1.yml'
 FLAT_YAML_2 = 'tests/fixtures/file2.yml'
-FLAT_JSON_ANSWER = 'tests/fixtures/flat_json_answer'
+NESTED_JSON_1 = 'tests/fixtures/nested_file1.json'
+NESTED_JSON_2 = 'tests/fixtures/nested_file2.json'
+NESTED_YML_1 = 'tests/fixtures/nested_file1.yml'
+NESTED_YML_2 = 'tests/fixtures/nested_file2.yml'
 
+FLAT_STYLISH_ANSWER = 'tests/fixtures/flat_stylish_answer'
+NESTED_STYLISH_ANSWER = 'tests/fixtures/nested_stylish_answer'
+
+cases = [
+    (FLAT_JSON_1, FLAT_JSON_2, FLAT_STYLISH_ANSWER),
+    (FLAT_YAML_1, FLAT_YAML_2, FLAT_STYLISH_ANSWER),
+    (NESTED_JSON_1, NESTED_JSON_2, NESTED_STYLISH_ANSWER),
+    (NESTED_YML_1, NESTED_YML_2, NESTED_STYLISH_ANSWER)
+]
 
 def get_file_content(file):
     with open(file) as file:
@@ -14,10 +26,12 @@ def get_file_content(file):
     return content
 
 
-def test_gen_diff():
-    assert gen_diff(FLAT_JSON_1, FLAT_JSON_2) == get_file_content(FLAT_JSON_ANSWER)
-    assert gen_diff(FLAT_YAML_1, FLAT_YAML_2) == get_file_content(FLAT_JSON_ANSWER)
+@pytest.mark.parametrize('file1,file2,answer', cases)
+def test_gen_diff(file1, file2, answer):
+    assert gen_diff(file1, file2) == get_file_content(answer)
 
+
+def test_exeption():
     with pytest.raises(Exception) as e:
-        gen_diff(FLAT_YAML_1, FLAT_JSON_ANSWER)
+        gen_diff(FLAT_YAML_1, FLAT_STYLISH_ANSWER)
     assert str(e.value) == 'Wrong file format!'
