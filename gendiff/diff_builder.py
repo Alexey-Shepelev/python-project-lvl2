f68635aa72fac2c4e8ade804475b75464db30bda
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-06-16 01:07:48 +0300

    Step 6. Intermediate commit(not working).

---
+++ gendiff/diff_builder.py
@@ -0,0 +1,32 @@
+def get_diff(data_1: dict, data_2: dict) -> dict:
+    union_dict = dict()
+    for key in sorted(data_1.keys() | data_2.keys()):
+        if key not in data_1:
+            union_dict[key] = {
+                'type': 'added',
+                'value': data_2[key]
+            }
+        elif key not in data_2:
+            union_dict[key] = {
+                'type': 'removed',
+                'value': data_1[key]
+            }
+        elif data_1[key] == data_2[key]:
+            union_dict[key] = {
+                'type': 'unchanged',
+                'value': data_1[key]
+            }
+        elif isinstance(data_1[key], dict) and isinstance(data_2[key], dict):
+            union_dict[key] = {
+                'type': 'nested',
+                'value': get_diff(data_1[key], data_2[key])
+            }
+        elif data_1[key] != data_2[key]:
+            union_dict[key] = {
+                'type': 'changed',
+                'value': {
+                    'old_value': data_1[key],
+                    'new_value': data_2[key]
+                }
+            }
+    return union_dict
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -9,7 +9,9 @@ def main():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument('-f', '--format',
+                        default='stylish',
+                        help='set format of output')
     args = parser.parse_args()
     diff = gen_diff(args.first_file, args.second_file)
     print(diff)
