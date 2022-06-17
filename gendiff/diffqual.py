e8aa9c6aed677af0cf4a51d73ab4e45884dd8135
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-06-17 22:47:05 +0300

    Step 6. Updated diffqual.py. Core logic moved to other files.

--- gendiff/diffqual.py
+++ gendiff/diffqual.py
@@ -1,22 +1,10 @@
-import json
 from gendiff.file_parser import parser
+from gendiff.diff_builder import get_diff
+from gendiff.stylish import get_stylish
 
 
-def gen_diff(file_1, file_2):
+def gen_diff(file_1, file_2, format='stylish'):
     content_1 = parser(file_1)
     content_2 = parser(file_2)
-    diff = dict()
-    for key in sorted(content_1.keys() | content_2.keys()):
-        if key not in content_1 and key in content_2:
-            diff['+ ' + key] = content_2[key]
-        elif key in content_1 and key not in content_2:
-            diff['- ' + key] = content_1[key]
-        elif content_1[key] == content_2[key]:
-            diff['  ' + key] = content_1[key]
-        else:
-            diff['- ' + key] = content_1[key]
-            diff['+ ' + key] = content_2[key]
-
-    return json.dumps(
-        diff, separators=('', ': '), indent=2
-    ).replace('"', '')
+    diff = get_diff(content_1, content_2)
+    return get_stylish(diff)
