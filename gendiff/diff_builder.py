a4299e5c799eae95b1f0511a170e2dc719bdbcd6
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-06-21 20:25:08 +0300

    Rev. #1. Update diff_builder.py.

--- gendiff/diff_builder.py
+++ gendiff/diff_builder.py
@@ -1,32 +1,32 @@
 def get_diff(data_1, data_2):
-    union_dict = dict()
+    diff = dict()
     for key in sorted(data_1.keys() | data_2.keys()):
         if key not in data_1:
-            union_dict[key] = {
+            diff[key] = {
                 'type': 'added',
                 'value': data_2[key]
             }
         elif key not in data_2:
-            union_dict[key] = {
+            diff[key] = {
                 'type': 'removed',
                 'value': data_1[key]
             }
         elif data_1[key] == data_2[key]:
-            union_dict[key] = {
+            diff[key] = {
                 'type': 'unchanged',
                 'value': data_1[key]
             }
         elif isinstance(data_1[key], dict) and isinstance(data_2[key], dict):
-            union_dict[key] = {
+            diff[key] = {
                 'type': 'nested',
                 'value': get_diff(data_1[key], data_2[key])
             }
-        else:   # data_1[key] != data_2[key]
-            union_dict[key] = {
+        else:
+            diff[key] = {
                 'type': 'changed',
                 'value': {
                     'old_value': data_1[key],
                     'new_value': data_2[key]
                 }
             }
-    return union_dict
+    return diff
