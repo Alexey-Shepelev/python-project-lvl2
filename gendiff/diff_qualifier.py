5df6db7a10f0613597df06b217559c5d70c4bc07
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-06-21 21:40:53 +0300

    Rev. #1. Update diff_qualifier.py & file_parser.py.

--- gendiff/diff_qualifier.py
+++ gendiff/diff_qualifier.py
@@ -1,10 +1,18 @@
 from gendiff.file_parser import parse
 from gendiff.diff_builder import get_diff
 from gendiff.formaters import get_formatted
+import os.path
+
+
+def get_data(file):
+    _, ext = os.path.splitext(file)
+    ext = ext.lstrip('.')
+    data = parse(file, ext)
+    return data
 
 
 def generate_diff(file_1, file_2, format_='stylish'):
-    content_1 = parse(file_1)
-    content_2 = parse(file_2)
+    content_1 = get_data(file_1)
+    content_2 = get_data(file_2)
     diff = get_diff(content_1, content_2)
     return get_formatted(diff, format_)
--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -2,9 +2,9 @@ import json
 import yaml
 
 
-def parse(file):
-    if file.endswith('.json'):
+def parse(file, file_format):
+    if file_format == 'json':
         return json.load(open(file))
-    elif file.endswith('.yml') or file.endswith('.yaml'):
+    elif file_format in ('yml', 'yaml'):
         return yaml.safe_load(open(file))
     raise Exception('Wrong file format!')
