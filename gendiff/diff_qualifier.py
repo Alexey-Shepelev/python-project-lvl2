7a1b55e20279aee41982be3917ac06dd5fdf2d8a
Alexey-Shepelev <alexey.sailor@gmail.com>
2022-06-22 19:38:00 +0300

    Rev. #2. Update diff_qualifier.py.

--- gendiff/diff_qualifier.py
+++ gendiff/diff_qualifier.py
@@ -12,8 +12,8 @@ def get_data(file):
     return output
 
 
-def generate_diff(file_1, file_2, format_='stylish'):
+def generate_diff(file_1, file_2, format_name='stylish'):
     content_1 = get_data(file_1)
     content_2 = get_data(file_2)
     diff = get_diff(content_1, content_2)
-    return get_formatted(diff, format_)
+    return get_formatted(diff, format_name)
